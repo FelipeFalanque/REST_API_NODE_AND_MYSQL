ffc643567eb09cead23101c7178e0ebaab446c41
Cesar Felipe Falanque <felipefalanque@gmail.com>
2021-01-06 20:09:04 -0300

    Aula 10 post com arquivo

--- REST/app.js
+++ REST/app.js
@@ -7,6 +7,9 @@ const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
+
+
 app.use(bodyParser.urlencoded({ extended: false }));  // apenas dados simples
 app.use(bodyParser.json()); // json de entrada no body
 
