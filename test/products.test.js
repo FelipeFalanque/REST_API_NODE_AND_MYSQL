330c1051db51e5769917cf6401121a1689dc0009
Cesar Felipe Falanque <felipefalanque@gmail.com>
2021-01-12 12:37:39 -0300

    Test, funcionando, havera refatoração

--- test/products.test.js
+++ test/products.test.js
@@ -1,19 +1,20 @@
 
+process.env.MYSQL_USER = "root";
+process.env.MYSQL_PASSWORD = "root";
+process.env.MYSQL_DATABASE = "curso_node";
+process.env.MYSQL_HOST = "localhost";
+process.env.MYSQL_PORT = 3306;
+process.env.JWT_KEY = "segredo";
+process.env.URL_API = "http://localhost:3000/";
+
 const request = require('supertest');
 const app = require('../app');
 
+describe(`Sample Test`, () => {
 
-describe('Sample Test', () => {
-
-
-
-    it('Get product list', async () => {
+    it(`Get product list`, async (done) => {
         const res = await request(app).get('/products');
-
-        console.log(res.body);
-
         expect(res.body);
-
-    });
-
-});
+        done();
+     })
+ })
